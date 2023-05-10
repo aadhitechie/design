5e43faea481ac9560fd22e84aaa75fd7af83c344
Adarsh M <m_adarsh@40bears.com>
2023-05-10 12:08:38 +0530

    reels refresh with new images

--- lib/view/reels_screen/reels_screen.dart
+++ lib/view/reels_screen/reels_screen.dart
@@ -1,12 +1,20 @@
 import 'package:designs/view/inbox_screen/inbox_screen.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class ReelsScreen extends StatelessWidget {
+class ReelsScreen extends StatefulWidget {
   const ReelsScreen({super.key});
 
   @override
+  State<ReelsScreen> createState() => _ReelsScreenState();
+}
+
+class _ReelsScreenState extends State<ReelsScreen> {
+  @override
   Widget build(BuildContext context) {
+    final imageUrl = 'https://source.unsplash.com/random/800x1200?film&';
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -48,153 +56,29 @@ class ReelsScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context) => InboxScreen()));
-},
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => InboxScreen()));
+            },
             icon: const Icon(
               CupertinoIcons.search,
             ),
           )
         ],
       ),
-      body: PageView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: lisOfImages.length,
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              Positioned.fill(
-                child: Image.network(
-                  lisOfImages[index],
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                  bottom: 25,
-                  left: 23,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white),
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.all(2),
-                              child: CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                    'https://images.unsplash.com/photo-1682917265562-139c5aa7070c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Carla Bellucci',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .copyWith(color: Colors.white),
-                                ),
-                                Text(
-                                  '@carla_bellucci',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Text(
-                            "GYM TIME! Join my live now to watch along! Xx I will be posting something special later tonight.",
-                            style: Theme.of(context).textTheme.labelSmall),
-                      )
-                    ],
-                  )),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.favorite),
-                      ),
-                      Text(
-                        '13.6k',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.comment_rounded),
-                      ),
-                      Text(
-                        '163',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.bookmark),
-                      ),
-                      Text(
-                        '5416',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.share_rounded),
-                      ),
-                      Text(
-                        '123',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.currency_exchange),
-                      ),
-                      Text(
-                        'Tip',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        onPressed: () {},
-                        icon: const Icon(Icons.badge),
-                      ),
-                      Text(
-                        'Buy',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          );
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            
+          });
         },
+        child: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: lisOfImages.length,
+          itemBuilder: (context, index) {
+            return ReelsWidget(imageUrl: imageUrl,);
+          },
+        ),
       ),
       extendBodyBehindAppBar: true,
       bottomNavigationBar: BottomNavigationBar(
@@ -226,6 +110,150 @@ class ReelsScreen extends StatelessWidget {
               label: ''),
         ],
       ),
+    );
+  }
+}
+
+class ReelsWidget extends StatefulWidget {
+  const ReelsWidget({super.key, required this.imageUrl});
+  final String imageUrl;
+  @override
+  State<ReelsWidget> createState() => _ReelsWidgetState();
+}
+
+class _ReelsWidgetState extends State<ReelsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image.network('${widget.imageUrl}${DateTime.now().millisecondsSinceEpoch}%27',fit: BoxFit.cover,),
+        ),
+        Positioned(
+            bottom: 25,
+            left: 23,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(2),
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://images.unsplash.com/photo-1682917265562-139c5aa7070c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Carla Bellucci',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge!
+                                .copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            '@carla_bellucci',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall!
+                                .copyWith(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                      "GYM TIME! Join my live now to watch along! Xx I will be posting something special later tonight.",
+                      style: Theme.of(context).textTheme.labelSmall),
+                )
+              ],
+            )),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.favorite),
+                ),
+                Text(
+                  '13.6k',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.comment_rounded),
+                ),
+                Text(
+                  '163',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.bookmark),
+                ),
+                Text(
+                  '5416',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.share_rounded),
+                ),
+                Text(
+                  '123',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.currency_exchange),
+                ),
+                Text(
+                  'Tip',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  icon: const Icon(Icons.badge),
+                ),
+                Text(
+                  'Buy',
+                  style: Theme.of(context).textTheme.labelSmall,
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
